83e1ab3729a6d228db5c68cc588ebc573771cdc5
ncacioni <nahuelcacioni@gmail.com>
2022-02-01 22:32:16 -0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,33 @@
+var navbar = document.getElementById('navbar');
+var menu = document.getElementById('menu');
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menu.offsetTop) {
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
+
+var menuBtn = document.getElementById('menuBtn');
+var sideNav = document.getElementById('sideNav');
+var mobilemenu = document.getElementById('mobilemenu');
+
+sideNav.style.right = "-250px";
+
+menuBtn.onclick = function() {
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0";
+        mobilemenu.src = "../images/close.png";
+    }
+    else {
+        sideNav.style.right = "-250px";
+        mobilemenu.src = "../images/menu.png";
+    }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1000,
+	speedAsDuration: true
+});
