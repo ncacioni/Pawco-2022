8b92ff43b3c6c7648f2f54263fdd2c60b4fa81ab
ncacioni <nahuelcacioni@gmail.com>
2022-02-06 14:50:27 -0300

    mobile changes

--- js/js.js
+++ js/js.js
@@ -31,3 +31,16 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000,
 	speedAsDuration: true
 });
+
+
+var navbar = document.getElementById('navbar');
+var menu = document.getElementById('menu');
+
+window.onscroll = function(){
+    if(window.pageYOffset >= menu.offsetTop) {
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
